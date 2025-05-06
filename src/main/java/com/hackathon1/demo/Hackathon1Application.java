36e9ebdd0720abd75cc1555cfb5778f7dda0f212
Adelphos21 <esquelat@gmail.com>
2025-05-06 16:34:25 -0500

    Initial commit

---
+++ src/main/java/com/hackathon1/demo/Hackathon1Application.java
@@ -0,0 +1,13 @@
+package com.hackathon1.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hackathon1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hackathon1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon1/demo/Hackathon1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon1.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hackathon1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
